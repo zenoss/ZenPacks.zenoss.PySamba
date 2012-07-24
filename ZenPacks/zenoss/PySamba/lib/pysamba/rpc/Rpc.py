924ba75fdfdfc1bc9780166314494ea997759343
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.PySamba@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/PySamba/lib/pysamba/rpc/Rpc.py
+++ ZenPacks/zenoss/PySamba/lib/pysamba/rpc/Rpc.py
@@ -1,15 +1,12 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008-2010, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2, or (at your
-# option) any later version, as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2008-2010, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
 
 __doc__="""
 Define a simple API for performing a sequence of RPC calls to a
@@ -173,4 +170,3 @@ class Rpc(object):
             return
         library.composite_continue_rpc(ctx, rpc_ctx, continue_rpc, ctx)
         return cb.deferred
-
--- src/wmi/Samba/source/torture/dcom/dcom.c
+++ src/wmi/Samba/source/torture/dcom/dcom.c
@@ -1,21 +1,12 @@
-/*
-   DCOM torture tester
-   Copyright (C) Zenoss, Inc. 2008
+/*****************************************************************************
+ * 
+ * Copyright (C) Zenoss, Inc. 2008, all rights reserved.
+ * 
+ * This content is made available according to terms specified in
+ * License.zenoss under the directory where your Zenoss product is installed.
+ * 
+ ****************************************************************************/
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
 
 #include "includes.h"
 #include "auth/credentials/credentials.h"
@@ -396,4 +387,3 @@ NTSTATUS torture_dcom_init(void)
 
     return NT_STATUS_OK;
 }
-
