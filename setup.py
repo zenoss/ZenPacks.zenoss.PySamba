f1aba59912ac3ef587e87129e5568827691a0187
bedwards <bedwards@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-27 20:53:07 +0000

    Fixes ZEN-2725. Reviewed by Danny. Add marker egg to detect version.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.PySamba@62062 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.PySamba']
-INSTALL_REQUIRES = []
+INSTALL_REQUIRES = ['Zenoss >= 4.1.70']
 COMPAT_ZENOSS_VERS = '>=4.1.70'
 PREV_ZENPACK_NAME = 'PySamba'
 # STOP_REPLACEMENTS
